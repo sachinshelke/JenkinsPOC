fc5dd3d496b61e0581917e66a4fe64cbaa60afe2
sachin <sachin.shelke@capco.com>
2017-12-21 11:47:41 +0530

    Added some code

--- app/src/main/java/com/sachinshelke/jenkinspoc/MainActivity.java
+++ app/src/main/java/com/sachinshelke/jenkinspoc/MainActivity.java
@@ -2,6 +2,7 @@ package com.sachinshelke.jenkinspoc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ((TextView)findViewById(R.id.txt)).setText("Hi This Damini's First Demo");
+
+
+
+
     }
 }
